22b545e4bbab7b6ec9c487058d8e24c5363c4b24
Genorius <qwerty7488@mail.ru>
2025-06-05 11:29:45 +0300

    Add files via upload

--- auth_db.py
+++ auth_db.py
@@ -1,8 +1,12 @@
-# Заглушка для хранения авторизации
-user_auth = {}
+import redis
+import os
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+r = redis.Redis.from_url(REDIS_URL)
 
 def save_user_auth(user_id, order_id):
-    user_auth[user_id] = order_id
+    r.set(f"user:{user_id}", order_id)
 
 def get_order_id_by_user_id(user_id):
-    return user_auth.get(user_id)
+    order_id = r.get(f"user:{user_id}")
+    return order_id.decode() if order_id else None
