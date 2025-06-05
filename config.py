fc938983339372ce61a373e48814c06341cbae45
Genorius <qwerty7488@mail.ru>
2025-06-05 18:42:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-API_KEY = os.getenv("API_KEY")
-CRM_URL = os.getenv("CRM_URL")
+API_KEY = "pDUAhKJaZZlSXnWtSberXS6PCwfiGP4D"
+CRM_URL = "https://valentinkalinovski.retailcrm.ru"
